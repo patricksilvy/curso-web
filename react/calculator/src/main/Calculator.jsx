67e47964548447f16ba12e1e8c9427410325a6a0
Patrick Silvy <pat28910oshi@gmail.com>
2023-01-11 09:37:19 -0300

    calculator logic

--- react/calculator/src/main/Calculator.jsx
+++ react/calculator/src/main/Calculator.jsx
@@ -8,7 +8,7 @@ const initialState = {
     displayValue: '0',
     clearDisplay: false,
     operation: null,
-    value: [0, 0],
+    values: [0, 0],
     current: 0
 }
 
@@ -28,7 +28,32 @@ export default class Calculator extends Component {
     }
 
     setOperation(operation) {
-        console.log(operation)
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true })
+        } else {
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+
+                if (isNaN(values[0]) || !isFinite(values[0])) {
+                    this.clearMemory()
+                    return
+                }
+            } catch(e) {
+                values[1] = 0
+            }
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values: values
+            })
+        }
     }
 
     addDigit(n) {
@@ -45,7 +70,7 @@ export default class Calculator extends Component {
         if (n !== '.') {
             const i = this.state.current
             const newValue = parseFloat(displayValue)
-            const values = [...this.state.value]
+            const values = [...this.state.values]
             values[i] = newValue
             this.setState({ values })
         }

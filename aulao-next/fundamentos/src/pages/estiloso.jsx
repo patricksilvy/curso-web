d43660b5c2330ad7a7129201bd7066b5a30efcf4
Patrick Silvy <pat28910oshi@gmail.com>
2023-01-18 07:53:51 -0300

    Css Modularizado

---
+++ aulao-next/fundamentos/src/pages/estiloso.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Estiloso.module.css'
+
+const Estiloso = () => {
+    return (
+        <div className={styles.roxo}>
+            <h1>Estilo usando CSS Módulos</h1>
+        </div>
+    )
+}
+
+export default Estiloso
